c7cc45d831fb01ec9655b674b7b9b1eccd794d50
Kayla Kantola <krkantola@gmail.com>
2025-02-25 20:01:47 -0500

    AccountsController#index and AccountsController#show

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,16 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: %i[ show ]
+
   def index
+    @accounts = Account.all
   end
 
   def show
+  end
+
+  private
+
+  def set_account
+    @account = Account.find(params[:id])
   end
 end
